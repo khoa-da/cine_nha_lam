1d105ff2b7d667bd2c72a3acaaa50dbd13975b83
khai732003 <khaithinhvui2003@gmail.com>
2023-11-28 23:53:26 +0700

    backend: update (order, orderDetail, ticket, voucher, voucherUsage, food) Entity

--- cinenhalamAll/cinanhalam/src/main/java/com/fap/cinanhalam/entity/VoucherEntity.java
+++ cinenhalamAll/cinanhalam/src/main/java/com/fap/cinanhalam/entity/VoucherEntity.java
@@ -1,11 +1,19 @@
 package com.fap.cinanhalam.entity;
 
+import com.fap.cinanhalam.converter.DateConverter;
 import jakarta.persistence.Column;
+import jakarta.persistence.Convert;
 import jakarta.persistence.Entity;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import jdk.jfr.Unsigned;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -25,17 +33,40 @@ public class VoucherEntity extends BaseEntity{
   @Column(name = "name", nullable = false)
   private String name;
 
-  @Column(name = "code", nullable = false)
+  @NotBlank
+  @Size(max=30)
+  @Column(name = "code", unique = true)
   private String code;
 
   @Column(name = "description", nullable = false)
   private String description;
 
-  @Column(name = "price", nullable = false)
-  private Double price;
+  @Unsigned
+  @Column(name = "value")
+  private Double value;
 
-  @Column(name = "status", nullable = false)
-  private boolean status;
+
+  @Temporal(TemporalType.DATE) // Specify that only the date part should be stored
+  // Specify that only the date part should be stored
+  @Column(name = "start_date")
+  @Convert(converter = DateConverter.class) // Use a custom converter
+  private Date startDate;
+
+
+  @Temporal(TemporalType.DATE) // Specify that only the date part should be stored
+  @Column(name = "end_date")
+  @Convert(converter = DateConverter.class) // Use a custom converter
+  private Date endDate;
+
+  @Column(name = "status")
+  private Boolean status;
+
+
+  @OneToMany(mappedBy = "promotion")
+  private List<OrderEntity> orders = new ArrayList<>();
+
+  @Column(name="quanity")
+  private int quantity;
 
   @OneToMany(mappedBy = "voucher")
   private List<VoucherUsageEntity> voucherUsages = new ArrayList<>();
